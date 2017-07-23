3a4706d7d373afe99f741f0034491510bde4849c
Siamois2016 <Josigam15@gmail.com>
2017-07-23 16:54:48 -0400

    update the sortlastname() method

--- B1_40002047/Employee.php
+++ B1_40002047/Employee.php
@@ -9,45 +9,45 @@
  */
 class Employee {
 
-    //Define private members
+//Define private members
 
-    private $employeeNumber;
-    private $firstName;
-    private $lastName;
+private $employeeNumber;
+private $firstName;
+private $lastName;
 
-    //define a constructor to use when creating an instance of this class
+//define a constructor to use when creating an instance of this class
 
-    public function __construct($employeeNumber, $firstName, $lastName) {
-        $this->employeeNumber = $employeeNumber;
-        $this->firstName = $firstName;
-        $this->lastName = $lastName;
-    }
+public function __construct($employeeNumber, $firstName, $lastName) {
+$this->employeeNumber = $employeeNumber;
+$this->firstName = $firstName;
+$this->lastName = $lastName;
+}
 
-    //Getters and setters
+//Getters and setters
 
-    function getEmployeeNumber() {
-        return $this->employeeNumber;
-    }
+function getEmployeeNumber() {
+return $this->employeeNumber;
+}
 
-    function getFirstName() {
-        return $this->firstName;
-    }
+function getFirstName() {
+return $this->firstName;
+}
 
-    function getLastName() {
-        return $this->lastName;
-    }
+function getLastName() {
+return $this->lastName;
+}
 
-    function setEmployeeNumber($employeeNumber) {
-        $this->employeeNumber = $employeeNumber;
-    }
+function setEmployeeNumber($employeeNumber) {
+$this->employeeNumber = $employeeNumber;
+}
 
-    function setFirstName($firstName) {
-        $this->firstName = $firstName;
-    }
+function setFirstName($firstName) {
+$this->firstName = $firstName;
+}
 
-    function setLastName($lastName) {
-        $this->lastName = $lastName;
-    }
+function setLastName($lastName) {
+$this->lastName = $lastName;
+}
 
 }
 
@@ -69,91 +69,76 @@ $employeeArray = array($emp1, $emp2, $emp3, $emp4, $emp5, $emp6);
 
 //Step4 & 7 : A function that displays all the employee in the array   
 function displayEmployee($empA) {
-   
-    foreach ($empA as $key =>$value) {
-        echo "Key= ".$key."value= ". $value->getEmployeeNumber() . ": " .
-        $value->getFirstName() . " " .
-        $value->getLastName() . "<br>";
-    }
-    echo "<hr>";
+
+foreach ($empA as $key => $value) {
+echo "Key= ".$key."value= ". $value->getEmployeeNumber() . ": " .
+$value->getFirstName() . " " .
+$value->getLastName() . "<br>";
+}
+echo "<hr>";
 }
 //Step4:Show  the unsorted array
 echo "<u>Unsorted List of Employee</u> <br><br>";
 displayEmployee($employeeArray);
 
-var_dump($employeeArray[0]);
+var_dump($employeeArray);
 
 echo "<hr>";
-echo $employeeArray[0]->getLastName();
-echo "<hr>";
+//echo $employeeArray[0]->getLastName();
 
-function sortByLastName($empA) {
-   $arraySorted = array();
-$size=count($empA);
-   
-   $i=0;
-   $temp = $empA[0];
-    foreach ($empA as $value) {
-        $actual = current($empA);
-        echo "actual = ".$actual->getLastName(). "<br>";
-      for($i==$size-1;$i>0;$i++){
-          next($empA);
-          if(strcmp($actual->getLastName(), $empA[$i]->getLastName())>=0){
-          $lastname= $actual->getLastName();
-          $firstname=$actual->getFirstName();
-          $id = $actual->getEmployeeNumber();
-          $employe = array($id,$firstname,$lastname);
-          $actual =  current($empA);
-        }
-         array_push($arraySorted, $employe);
-          
-      } 
-     
-      
-      
-       
-       
-        
-        
-    }
-    
+//array_intersect_assoc($arrayOfLastName, $empA);
+// var_dump($empA);
 
-    var_dump($arraySorted); 
-    echo "<hr>";
-     //array_intersect_assoc($arrayOfLastName, $empA);
-   // var_dump($empA);
-    
-}
-sortByLastName($employeeArray);
+
+//sortByLastName($employeeArray);
 //Step6: create a custom sort function. To sort employees by Lastname
-/*
-function sortByLastName($empA) {
-    $arrayOfLastName = array();
-    $size=  count($empA);
-   $current= reset($empA);
-   $next = next($empA);
-   
-   if(strcmp($current->getLastName(), $next->getLastName())>0){
-       
-       array_push($arrayOfLastName, $current);
-       
-   }
-    return $arrayOfLastName;
-    
+
+  function sortByLastName($empA) {
+  $tempArray = $empA;
+  $size=count($empA);
+  echo "size= ".$size;
+  $i=0;
+  $j=0;
+  do{
+   // for($i==($size-1);$i>0;$i--) {
+   reset($empA);
+        $str1=$empA[0]->getLastName();
+        $str2=$empA[$i]->getLastName();
+      if(strcmp($str1,$str2)>=0 && $i<($size-1) ){
+     //eitner the same or greater
+        $tempArray[$j]=$empA[$i]; 
+        next($empA);
+        $i++;
     }
-    
-   
-
-//Step5: usort statement
-usort($employeeArray, 'sortByLastName');
-
-//show the sorted list
-echo "<u>Sorted List of Employee</u><br> <br>";
-
-foreach ($employeeArray as $value) {
-        echo 
-        $value->getLastName() . "<br>";
+    if(strcmp($str1,$str2)<0 && $i<($size-1)){
+       $tempArray[$j]=$empA[$i+1]; 
+        next($empA);
+        $i++;
     }
+      
+  //}
+   array_push($tempArray, $tempArray[$j]);
+    $j++; 
+    
+  
+    }while($j<$size);
+ 
+      
+  //}
+ 
+  var_dump($tempArray);
+      
+//$arrayOfLastName = array();
+  }
+
+sortByLastName($employeeArray);
+
+  //Step5: usort statement
+  //usort($employeeArray, 'sortByLastName');
+
+  //show the sorted list
+  //echo "<u>Sorted List of Employee</u><br> <br>";
+
   
 
-*/
+ 
