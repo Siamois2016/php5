7326de56e7d4513af3b29a66300251b4a416054f
Siamois2016 <josigam15@gmail.com>
2017-07-31 09:48:32 -0400

    changes on phpclass3

--- PhpClass3/arrayMerge.php
+++ PhpClass3/arrayMerge.php
@@ -3,10 +3,10 @@
 $a  = array(8,3,5,2,99,3);
 $b= array(2,6);
 $c =  array_merge($a, $b);
-print_r($c);
+print_r($c); // print 6 at the end of the array
 echo "<hr>";
 $x= array(1,2);
 $y = array(2,3);
 $z = array_diff($y, $x);
 
-print_r($z);
+print_r($z);//print 3
