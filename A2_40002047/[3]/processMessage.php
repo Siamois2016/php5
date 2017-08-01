8dbae530e42138c6ddb0a19f678d43cb72e113fc
Siamois2016 <Josigam15@gmail.com>
2017-08-01 11:22:25 -0400

    assignment 2 - part3 :test ok . still showing field after submission.Need to hide the form when success

--- A2_40002047/[3]/processMessage.php
+++ A2_40002047/[3]/processMessage.php
@@ -6,11 +6,14 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
+   
     <body>
                 <div>
                     <form  action="" method="POST">
-                        <p> <label for="name">Your Name:</label><br><input type="text" id="name" name="name"/></p>
-                        <p><label for="message">Message:</label><br><input type="text" id="message" name="message"/></p>
+                        <p> <label for="name">Your Name:</label><br>
+                            <input  type="text" id="name" name="name" value="<?php if(isset($_POST['name'])){echo $_POST['name'];} ?>"/></p>
+                        <p><label for="message">Message:</label><br>
+                            <input  type="text" id="message" name="message" value="<?php if(isset($_POST['message'])){echo $_POST['message'];} ?>"/></p>
                         <p><input type="submit" value="Submit"/></p>
                         
                         
@@ -26,15 +29,17 @@
 //echo $_SERVER["PHP_SELF"];
 
 if (isset($_POST["name"]) && isset($_POST["message"])) {
-    //message
+   //if success hidde form
     if (!empty($_POST["name"]) && !empty($_POST["message"])) {
         echo "Thanks! your message has been submitted";
-        
+          
     } else {
+        //if no name
         if (empty($_POST["name"])) {
 
             echo "NAME  is not entered please enter your name.";
         }
+        //if no message
         if (empty($_POST["message"])) {
             echo "No MESSAGE has been entered.";
         }
