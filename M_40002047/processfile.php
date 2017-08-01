0386a14fb66b022ed075e6e8cf33bb7357d925e2
Siamois2016 <j_gamgo@CCE-LAB138-40.concordia.ca>
2017-08-01 18:45:58 -0400

    Midterm Exam Q1 : Answer

--- M_40002047/processfile.php
+++ M_40002047/processfile.php
@@ -4,9 +4,14 @@
  * Description of processfile
  *
  * @author j_gamgo
+ * step1 : put $_POST[] ->$myArray
+ *step2: myname ->$myName
+ * text* -> put into $textArray\
+ * read each text and put into the $textArray
  */
 
-var_dump($_POST);
+
+/*
 $list = array();
 $counter =0;
 echo "<hr>";
@@ -19,5 +24,43 @@ echo "<hr>";
       array_push($list, $v);
      
     }
+*/
+
+
+$textArray = array();
+$counter = 1;
+//extract the name
+if(!empty($_POST['myName'])){
+ $myName=$_POST['myName'];    
+}else{
+    echo "Error: my Name is not defined in POST";
+}
+
+//extract the aray values
+foreach ($_POST as $k =>$v) {
+   if(substr($k,0,4)=="text"){
+       array_push($textArray, $v);
+   } 
+}
+
+echo "Hello, $myName<br><br>";
+ echo "The values in order are: <br><br>";
+ sort($textArray);
+ foreach($textArray as $k=>$v){
+     echo $counter++.") $v <br>";
+    
+ }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
