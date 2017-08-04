63aeba46f730c7d0ede0e6cfe093e4db3dbada88
Siamois2016 <Josigam15@gmail.com>
2017-08-04 00:48:40 -0400

    +modify logic. Only display form with get, and process when post method is called

--- A2_40002047/[3]/processMessage.php
+++ A2_40002047/[3]/processMessage.php
@@ -1,87 +1,78 @@
-<?php 
-$name="";
-$message="";
-
-if($_SERVER['REQUEST_METHOD']=='POST')
-    {
-    
-      if(!(empty($_POST['name'])) && !(empty($_POST['message']))){
-          
-          $name = $_POST['name'];
-          $message = $_POST['message'];
-      
-      }else{
-          //if no name
-        if (empty($_POST['name'])) {
-
-            echo "<p style='color:red;'>NAME  is not entered please enter your name.</p>";
-            
-        }
-         //if no message
-        if (empty($_POST['message'])) {
-            echo "<p style='color:red;'>No MESSAGE has been entered.</p>";
-        } 
-         
-          
-      }
-       
-      
-        
-      
-    }
-
-
-?>
-
 <html>
     <head>
         <title>Message To Tech Support</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="messageStyle.css"/>
-        <script type='text/javascript'> 
        
-       function hideForm() {
-                document.getElementById('stickyform').style.display="none";
-             }
-            
-       </script>
     </head>
-   
+
     <body>
-                <div id="stickyform">
-                    <h3>Please enter a message to send to our technical support group</h3>
-                    <form  action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST" >
-                        <p> <label for="name">Your Name:</label><br><br>
-                            <input  type="text" id="name" name="name" value="<?php echo $name; ?>" size="50"/>
-                        </p>
-                        <p><label for="message">Message:</label><br><br>
-                            <input  type="text" id="message" name="message" value="<?php echo $message; ?>" size="50"/>
-                        </p>
-                        <p><input type="submit" value="Submit"/></p>
-                        
-                        
-                        
-            </form>
+
+        <?php
+        /*displayForm($n,$m):A function that displays the form , 
+         * whatever is entered in the other field still shows if this field is empty
+         * Method recieve the 2 values as parameter
+         *
+         */
+        
+        function displayForm($n,$m){
+            
+        ?>
+            <div id="stickyform">
+                <h3>Please enter a message to send to our technical support group</h3>
+
+                <form  action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" >
+                    <p> <label for="name">Your Name:</label><br><br>
+                        <input  type="text" id="name" name="name" value="<?php echo $n; ?>" size="50"/>
+                    </p>
+                    <p><label for="message">Message:</label><br><br>
+                        <input  type="text" id="message" name="message" value="<?php echo $m; ?>" size="50"/>
+                    </p>
+                    <p><input type="submit" value="Submit"/></p>
+                    
+
+
+                </form>  
+
+            </div>
+        <?php
+        }
+         //Display the form when going into it for the first time
+        
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            
+            displayForm("", "");
+        }
+        /*
+         * when the form is submitted: validation and processing
+         * if all fields are filled. display success message and Form disappears
+         * if there is no name or no message show the form and the warning message
+         * 
+         */
+        
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+            if (!(empty($_POST['name'])) && !(empty($_POST['message']))){
                 
+                echo "<p>Thanks! your message has been submitted</p>";
                 
-                </div>
+            } 
+            else{
+                
+                displayForm($_POST['name'], $_POST['message']);
+             
+                if (empty($_POST['name'])) {
+
+                    echo "<p>NAME  is not entered please enter your name.</p>";
+                }
+               
+                if (empty($_POST['message'])){
+                    
+                    echo "<p>No MESSAGE has been entered.</p>";
+                }
+            }
+        }
+        ?>
     </body>
 </html>
-
-
-
-<?php
-
-
-          
-   
-   //if success display message and  hidde form
-    if (!empty($name) && !empty($message)) {
-        echo "<p>Thanks! your message has been submitted</p>";
-        ?>
-         <script type='text/javascript'> hideForm()</script>
-         <?php
-         }
-
-
