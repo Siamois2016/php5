6c9b60c48ef60dfbb71632c33810e9a17979c402
Siamois2016 <Josigam15@gmail.com>
2017-08-06 15:07:27 -0400

    Q4: 2. remove of all numbers in a string

--- A2_40002047/[4]/replaceValueRegex.php
+++ A2_40002047/[4]/replaceValueRegex.php
@@ -1,7 +1,19 @@
 <?php
 
 /* 
- * 1. Repace all values of 1,2,3,4,5,6,7,8,9,0 with the respective word. (1 = “one”).
+ * 1. Repace all values of 1,2,3,4,5,6,7,8,9,0 
+ * with the respective word. (1 = “one”).
 2. Get rid of all numbers in a string. (“abc123abc” = “abcabc”).
  */
+//1
 
+$subject = [1,2,3,4,5,6,7,8,9,0];
+$pattern = ["one","two","three","four","five","six","seven","height","nine","zero"];
+
+
+//2
+$string = "abc123abc";
+$p = "/\d+/";
+$replacement ="";
+echo "<hr>";
+echo preg_replace($p, $replacement, $string);
