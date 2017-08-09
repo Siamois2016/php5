4f5aad594c3d3802d4ecc0c366e7b8e48ad060d1
Siamois2016 <j_gamgo@CCE-LAB138-40.concordia.ca>
2017-08-08 20:21:58 -0400

    completing part4.3

--- A2_40002047/[4]/replaceValueRegex.php
+++ A2_40002047/[4]/replaceValueRegex.php
@@ -1,17 +1,35 @@
 <?php
 
-/* 
- * 1. Repace all values of 1,2,3,4,5,6,7,8,9,0 
- * with the respective word. (1 = “one”).
-2. Get rid of all numbers in a string. (“abc123abc” = “abcabc”).
- */
-//1
+//1. Repace all values of 1,2,3,4,5,6,7,8,9,0 with the respective word. (1 = “one”, 2= "two",...).  
+function changeNumberToString($matches) {
+    switch ($matches[0]) {
+        case 1:
+            return "one";
+        case 2:
+            return "two";
+        case 3:
+            return "three";
+        case 4:
+            return "four";
+        case 5:
+            return "five";
+        case 6:
+            return "six";
+        case 7:
+            return "seven";
+        case 8:
+            return "eight";
+        case 9:
+            return "nine";
+          case 0:
+            return "zero";
+    }
+}
 
-$subject = [1,2,3,4,5,6,7,8,9,0];
-$pattern = ["one","two","three","four","five","six","seven","height","nine","zero"];
 
+echo preg_replace_callback("/\d/", 'changeNumberToString', "1,2,3,4,5,6,7,8,9,0");
 
-//2
+//2. Get rid of all numbers in a string. (“abc123abc” = “abcabc”).
 $string = "abc123abc";
 $p = "/\d+/";
 $replacement ="";
